aa022a44a6fe5bf304efb74b579684501431b4a9
cainesap <cainesap@gmail.com>
2019-04-10 15:04:20 +0100

    cut() update

--- R/useful_base.R
+++ R/useful_base.R
@@ -592,3 +592,7 @@ rep(1:4, each = 2)       # not the same.
 rep(1:4, each = 2, len = 4)    # first 4 only.
 rep(1:4, each = 2, len = 10)   # 8 integers plus two recycled 1's.
 rep(1:4, each = 2, times = 3)  # length 24, 3 complete replications
+
+
+# cut function to bin continuous values
+cut(values, seq(0,5,1), include.lowest=T)
